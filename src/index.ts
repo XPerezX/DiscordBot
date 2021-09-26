1f826a177cca07fd2990efe6e360d0a9fd69e53e
XPerezX <perez@startapp.one>
2021-09-26 14:21:11 -0300

    join and play command

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,51 @@
 import dotenv from "dotenv";
 import { Client, Intents } from "discord.js";
+import { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior } from "@discordjs/voice";
+import * as playdl from 'play-dl'
 
 dotenv.config();
 
-const client = new Client({ });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 client.once("ready", () => {
     console.log("Mãe ta on");
 });
 
-client.on("message", async interaction => {
+client.on("interactionCreate", async interaction => {
 	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
 	if (commandName === "p") {
+
 		await interaction.reply("Musica selecionada");
-		console.log(interaction.options.getString("link"));
+		const interactionUserChannel = interaction.guild?.members.cache.find((user) => user.id === interaction.user.id)?.voice.channel;
+		const ytbUrl = interaction.options.getString("link");
+
+		if(!interactionUserChannel || !ytbUrl) {
+			await interaction.reply("Não foi possivel entrar no canal")
+			return;
+		}
+
+		const voiceConnection = joinVoiceChannel({
+			guildId: interactionUserChannel.guildId,
+			channelId: interactionUserChannel.id,
+			adapterCreator: interactionUserChannel.guild.voiceAdapterCreator,
+			selfDeaf: false,
+		})
+
+		let stream = await playdl.stream(ytbUrl);
+
+        const resource = createAudioResource(stream.stream, { inputType: stream.type });
+		let player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Play
+            }
+        });
+		
+		voiceConnection.subscribe(player);
+		player.play(resource);
+		
 	} else if (commandName === 'server') {
         if (!interaction.guild) return;
         await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
--- src/resources/deploy-commands.mjs
+++ src/resources/deploy-commands.mjs
@@ -6,8 +6,7 @@ import { Routes } from "discord-api-types/v9";
 dotenv.config();
 
 const commands = [
-	new SlashCommandBuilder().setName("p").setDescription("toPlay").addStringOption(
-		options => options.setName("link").setDescription("the Link")),
+	new SlashCommandBuilder().setName("p").setDescription("toPlay").addStringOption(options => options.setName("play link").setDescription("ytb href to play songs")),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 ]
 	.map(command => command.toJSON());
